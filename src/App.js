daf84e3ac23d7205ddb5851fb7796e444c225751
Samory Sundjata <samory.sundjata@gmail.com>
2024-10-31 16:53:37 -0300

    Fetch and display student data from API in App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,11 +10,27 @@ import logoCadastro from "./assets/cadastro.jpg";
 
 function App() {
   const baseUrl = "http://localhost:7266/api/alunos";
+  const [data, setData] = useState([]);
+  const pedidoGet = async () => {
+    await axios
+      .get(baseUrl)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    pedidoGet();
+  });
+
   return (
     <div className="App">
       <br />
       <h3>Cadastro de Alunos</h3>
-      <header className="App-header">
+      <header>
         <image src={logoCadastro} alt="logoCadastro" />
         <button className="btn btn-success">Incluir Novo Aluno</button>
       </header>
@@ -26,7 +44,20 @@ function App() {
             <th>Operação</th>
           </tr>
         </thead>
-        <tbody>{/* exibir os dados */}</tbody>
+        <tbody>
+          {data.map((aluno) => (
+            <tr key={aluno.id}>
+              <td>{aluno.id}</td>
+              <td>{aluno.nome}</td>
+              <td>{aluno.email}</td>
+              <td>{aluno.idade}</td>
+              <td>
+                <button className="btn btn-primary">Editar</button>{" "}
+                <button className="btn btn-danger">Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
